185d1d6c2765c72888da06636e9c227d410b909a
Taneli Peltoniemi <taneli.peltoniemi@gmail.com>
2013-01-12 17:22:17 +0200

    Base of authentication server done, Server listens on port 9958

--- AuthenticationServer/Program.cs
+++ AuthenticationServer/Program.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace AuthenticationServer
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            AuthServerSocket Server = new AuthServerSocket();
+            Server.Initialize();
+
+            bool Running = true;
+            while (Running)
+            {
+                Console.Write("> ");
+                string Command = Console.ReadLine();
+
+                if (CheckCommand("quit", Command))
+                    Running = false;
+            }
+        }
+        private static bool CheckCommand(string Command, string Input)
+        {
+            return Command.ToLower() == Input.ToLower();
         }
     }
 }
--- NetworkLibrary/IPacketCipher.cs
+++ NetworkLibrary/IPacketCipher.cs
@@ -2,8 +2,11 @@
 {
     public interface IPacketCipher
     {
+        void Initialize();
         void Decrypt(byte[] Data, int Size);
         void Encrypt(byte[] Data, int Size);
+        void GenerateKeys(uint Key1, uint Key2);
+
         void Dispose();
     }
 }
